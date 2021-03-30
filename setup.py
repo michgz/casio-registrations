f23d4b81256dc546b71baa55dc4c0d6b2a0c64ff
michgz <mdr.nz11@gmail.com>
2021-03-30 18:44:39 +0100

    Get distribution (building/installing) working

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name = 'casio-registrations',
+    version = '1.0',
+    description = 'For reading and writing Casio registration bank (.RBK) files',
+    long_description = open('README.md').read(),
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/michgz/casio-registrations',
+    project_urls =
+        ['Bug Tracker = https://github.com/michgz/casio-registrations/issues'],
+    classifiers =
+        ['Programming Language :: Python :: 3',
+         'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+         'Operating System :: OS Independent' ],
+    license = 'LGPLv2+',
+    packages = ['casio_rbk'],
+    package_data = {'casio_rbk': ['patch_data/CT-X5000 tone.csv', 'patch_data/README.md']}
+)
