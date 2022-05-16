9d0cae8a951908e5c9a0d641c261db538890e83a
michgz <mdr.nz11@gmail.com>
2022-05-16 20:17:26 +0100

    Add CT-X870IN as a keyboard. Upversion to 1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = 'casio-registrations',
-    version = '1.2',
+    version = '1.5',
     description = 'For reading and writing Casio registration bank (.RBK) files',
     long_description = open('README.md').read(),
     long_description_content_type = 'text/markdown',
